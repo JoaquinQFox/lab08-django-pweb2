ec87b5a7657086088afcda23e65da7262716440c
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-06-03 20:12:08 -0500

    Se agregan apps creadas a settings de carpeta de configuración

--- lab_project/settings.py
+++ lab_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dbrelations',
+    'communications',
 ]
 
 MIDDLEWARE = [
