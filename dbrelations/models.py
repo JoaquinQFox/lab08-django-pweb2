c7c5180226255a04979f8bc2980e75fd260bdd3c
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-06-03 20:07:00 -0500

    Se definio las tablas Lenguaje y Framework en models de la app dbrelations

--- dbrelations/models.py
+++ dbrelations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Lenguaje(models.Model):
+    nombre = models.CharField(max_length=10)
+
+class Framework(models.Model):
+    nombre = models.CharField(max_length=10)
+    lenguaje = models.ForeignKey(Lenguaje, on_delete=models.CASCADE)
