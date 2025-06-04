3796c37372ebfe6c9ee2841945adb4350849dbd6
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-06-03 20:28:56 -0500

    Se modifica __str__ de clase Lenguaje y Framework de app dbrelations

--- dbrelations/models.py
+++ dbrelations/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Lenguaje(models.Model):
     nombre = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.nombre
+
 class Framework(models.Model):
     nombre = models.CharField(max_length=10)
     lenguaje = models.ForeignKey(Lenguaje, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.lenguaje})"
