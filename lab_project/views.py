bfccce34822755995245af1ae937479d34f8e73e
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-06-08 15:30:11 -0500

    Se agrego pagina pdf index con form para ingresar con form las partes del pdf creado

--- lab_project/views.py
+++ lab_project/views.py
@@ -1,20 +1,26 @@
 from django.http import HttpResponse
 
 from django.views.generic import View
-
 from django.template.loader import get_template
+from django.shortcuts import render
+
+import datetime
 
 from .renderers import render_to_pdf
 
 class GeneratePdf(View):
     def get(self, request, *args, **kwargs):
         template = get_template('pdf/invoice.html')
+
         context = {
-            "invoice_number": 123,
-            "customer_name": "Joaquin",
-            "amount": 500,
-            "date": "Today",
+            "invoice_number": request.GET.get('numero'),
+            "customer_name": request.GET.get('nombre'),
+            "amount": request.GET.get('monto'),
+            "date": datetime.date.today(),
         }
 
         pdf = render_to_pdf('pdf/invoice.html', context)
         return HttpResponse(pdf, content_type='application/pdf')
+
+def index(request):
+    return render(request, 'pdf/create_pdf.html', {})
